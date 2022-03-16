3a9acdb9d5990cfbc90a6be711edc59997ef7562
Mariya Suhova <mariya.suhova@icloud.com>
2022-03-16 14:10:38 +0400

    issue3 task1

--- task_1/solution.js
+++ task_1/solution.js
@@ -44,6 +44,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+    let newSum = oldSum + difference;
+
+    let newSumText = `${newSum} ₽`;
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
