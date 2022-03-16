0479471efbf48a247eccb31654019b276207126f
Mariya Suhova <mariya.suhova@icloud.com>
2022-03-16 15:21:17 +0400

    issue1 task2

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,6 +16,15 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+    let shippingSum;
+    if (productsSum = 0) {
+        shippingSum = 0;
+    } else if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
+        
     // Конец решения задания №2.1.
 
     return shippingSum;
