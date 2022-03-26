776fd6a8280e196a10f5aacbc61d8f2836bf9dee
Mariya Suhova <mariya.suhova@icloud.com>
2022-03-26 23:07:49 +0400

    update final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -41,8 +41,7 @@ function sendRequest(name, phone, address, goods, sum) {
     data.client = `${name} ${phone}`;
  
     for (let i = 0; i < countOfGoods; i += 1) { 
-        myGoods = {title: goods[i].title, count: goods[i].count}
-        data.goods.push(myGoods);
+        data.goods.push ({title: goods[i].title, count: goods[i].count});
     }
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
