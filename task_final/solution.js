9d566f28f3889f4939940f8a594c6400ddf4a97b
Mariya Suhova <mariya.suhova@icloud.com>
2022-03-26 22:54:02 +0400

    final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,24 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, order: {address, sum}, goods: []};
-
+    let data = {order: {}, goods: []};
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);}
-    for (let i = 0; i < goods.length; i++) {
-        data.goods.push({title: goods[i].title, count: goods[i].count})
+        data.client = ${name} ${phone};
+ 
+    for (let i = 0; i < countOfGoods; i += 1) { 
+        myGoods = {title: goods[i].title, count: goods[i].count}
+        data.goods.push(myGoods);
     }
+        data.order.address = ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat};
+        data.order.sum = sum;
 
-    data.order.address = 'ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}';
-    data.order.sum = name + phone + address + goods + sum;;
+    let jsonData = JSON.stringify({data});
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-    
     return jsonData;
-    return JSON.stringify({ data });
-
 }
